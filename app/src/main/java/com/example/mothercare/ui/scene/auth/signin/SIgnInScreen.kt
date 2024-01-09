3a2a6326f31c4bfa487c661901cc7479963047f1
ufuomaisaac <ejemudaroufuoma@gmail.com>
2024-01-09 11:25:55 +0100

    User: Input Email

--- app/src/main/java/com/example/mothercare/ui/scene/auth/signin/SIgnInScreen.kt
+++ app/src/main/java/com/example/mothercare/ui/scene/auth/signin/SIgnInScreen.kt
@@ -1,18 +1,36 @@
 package com.example.mothercare.ui.scene.auth.signin
 
 import android.graphics.drawable.Icon
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.text.KeyboardActions
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material.OutlinedTextField
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.KeyboardArrowLeft
 import androidx.compose.material3.CenterAlignedTopAppBar
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.focus.onFocusChanged
+import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.text.input.ImeOptions
+import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.mothercare.theme.MotherCareTheme
+import com.example.mothercare.theme.Typography
+import com.example.mothercare.ui.scene.auth.state.EmailState
+import com.example.mothercare.ui.scene.auth.state.TextFieldState
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -37,8 +55,58 @@ fun SignInSIgnUpAppBar(
 }
 
 @Composable
-fun email() {
+fun email(
+    emailState: TextFieldState =  remember {EmailState()},
+    imeAction: ImeAction = ImeAction.Next,
+    onImeAction: () -> Unit = {}
+    ) {
+    OutlinedTextField(
+        value = emailState.text.toString(),
+        onValueChange = {
+            emailState.text = it
+        },
+        label = {
+            Text(text = "email",
+                style = Typography.bodyMedium)
+        },
+        modifier = Modifier
+            .fillMaxWidth()
+            .onFocusChanged { focusState ->
+                emailState.onFocusChange(focusState.isFocused)
+                if (!focusState.isFocused) {
+                    emailState.enableShowErrors()
+                }
+            },
+        textStyle = MaterialTheme.typography.bodyMedium,
+        isError = emailState.showErrors(),
+        keyboardOptions = KeyboardOptions.Default.copy(
+            imeAction = imeAction,
+            keyboardType = KeyboardType.Email
+        ),
+        keyboardActions = KeyboardActions(
+            onDone = {
+                onImeAction()
+            }
+        ),
+        singleLine = true
+    )
 
+    emailState.getError()?.let { error -> TextFieldError(textError = error) }
+}
+
+/**
+ * To be removed when [TextField]s support error
+ */
+@Composable
+fun TextFieldError(textError: String) {
+    Row(modifier = Modifier.fillMaxWidth()) {
+        Spacer(modifier = Modifier.width(16.dp))
+        Text(
+            text = textError,
+            modifier = Modifier.fillMaxWidth(),
+            color = MaterialTheme.colorScheme.error
+        )
+    }
 }
 
 @Preview()
@@ -46,8 +114,7 @@ fun email() {
 fun SignInPreview() {
     MotherCareTheme {
         Surface {
-            SignInSIgnUpAppBar(topAppBarTitle = "Compose") {
-            }
+            email()
         }
     }
 }
