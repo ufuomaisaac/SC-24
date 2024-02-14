d8596add7b66a6c47b0c883c4c50626d9422d04e
ufuomaisaac <ejemudaroufuoma@gmail.com>
2024-02-14 15:52:03 +0100

    profile

--- app/src/main/java/com/example/mothercare/ui/scene/profile/UserProfile.kt
+++ app/src/main/java/com/example/mothercare/ui/scene/profile/UserProfile.kt
@@ -1,11 +1,13 @@
 package com.example.mothercare.ui.scene.profile
 
+import androidx.compose.foundation.layout.Column
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
 
 @Composable
-fun Profile() {
+fun UserProfile() {
+    Column() {
 
+
+    }
 
 }
